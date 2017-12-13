3fb1d88b7940b4d9a8090373ab2ae73f95b003e8
Admin <admin@Admins-Mac.local>
2017-12-13 17:11:24 +0500

    Initial Commit

---
+++ GoMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoMaps
+//
+//  Created by Admin on 13/12/2017.
+//  Copyright © 2017 globiaTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
